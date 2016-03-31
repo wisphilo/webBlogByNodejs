34fec79a7bc45b0d832adec0c9c52eb0da3eb80b
cheng ming <wisphilo@gmail.com>
2016-03-30 21:20:35 -0700

    basic blog

--- blog/app.js
+++ blog/app.js
@@ -24,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.get('/nswbmw', function (req, res) {
+  res.send('hello,world!');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
